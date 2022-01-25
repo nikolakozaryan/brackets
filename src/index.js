3df8c880a070e83f8c03ce8a48077d49099bbeb0
Nikolay Kozyrev <kolyak770@gmail.com>
2022-01-25 23:57:46 +0300

    start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) return false;
+
+  bracketsConfig = bracketsConfig.map(item => typeof +item[0] === 'number' ? item[0] + item[1] : `[\\]${item[0]}[\\]${item[1]}`)
+  let regex = new RegExp(`${bracketsConfig.join('|')}`, 'g')
+
+  console.log(bracketsConfig, regex)
+  
+  while (regex.test(str)) {
+    str = str.replace(regex, '')
+  }
+  
+  return !str.length
 }
