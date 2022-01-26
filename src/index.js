5f7c9f5708fdaadb1f25cbe38c63218efe4d8899
Николай Козырев <n.kozyrev@sudexpert.local>
2022-01-26 09:30:29 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => !isNaN(item[0] - item[0]) ? `${item[0]}${item[1]}` : `\\${item[0]}\\${item[1]}`)
+  let regex = new RegExp(`${bracketsConfig.join('|')}`, 'g')
+
+  while (regex.test(str)) {
+    str = str.replace(regex, '')
+  }
+
+  return !str.length
 }
